363b82ef99e16a58863cdda098ac64a5c8a7534a
hyansuper <hyansuper@foxmail.com>
2020-06-27 01:18:50 +0800

    add setup.py

---
+++ README.md
@@ -0,0 +1,5 @@
+# rcute-ai
+Simple wrapper over some python libs for image/audio detection/recognition etc to facilitate rcute robots(eg. [rcute-cozmars](https://github.com/hyansuper/rcute-cozmars))
+
+## Examples
+see https://github.com/hyansuper/rcute-ai/examples
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("./README.md", 'r') as f:
+    long_description = f.read()
+
+with open('./requirements.txt', 'r') as f:
+    requirements = [a.strip() for a in f]
+
+setuptools.setup(
+    name="rcute-ai",
+    version="1.0",
+    author="Huang Yan",
+    author_email="hyansuper@foxmail.com",
+    description="Simple wrapper over some python libs for image/audio detection/recognition etc",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyansuper/rcute-ai",
+    packages=['rcute_ai'],
+    install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
